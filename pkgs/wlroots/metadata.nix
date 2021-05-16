f599e00647919809dcedab989e695fce6e48c700
Rouven Czerwinski <rouven@czerwinskis.de>
2021-05-16 07:24:08 +0200

    wlroots: switch to dmabuf reuse branch
    Signed-off-by: Rouven Czerwinski <rouven@czerwinskis.de>

--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,6 +1,6 @@
 {
-  repo_git = "https://github.com/swaywm/wlroots";
-  branch = "master";
-  rev = "9f211b5dd4b2326f71831ea2a9cb42652029e3ec";
-  sha256 = "sha256-AF1OJi53FqdGefoomOKIs5ckGiLz+1uvBTMappSvcMw=";
+  repo_git = "https://github.com/emersion/wlroots";
+  branch = "linux-dmabuf-reuse-buffer";
+  rev = "e84e9641852502ba0d830c2c676a9d36252a0ee7";
+  sha256 = "sha256-zC6kBzZRiUTRPRDTPFEwgdfk3zUKS6GANJCcJtxcda8=";
 }
